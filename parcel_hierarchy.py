dede5eecdd98923d6f8b9135c9f3bdf21cf004a4
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2022-10-25 17:16:36 -0400

    parcel_dendrogram

--- parcel_hierarchy.py
+++ parcel_hierarchy.py
@@ -19,6 +19,32 @@ import seaborn as sb
 import sys
 import pickle
 from evaluate import *
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.cluster import AgglomerativeClustering
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
 
 base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion'
 if not Path(base_dir).exists():
@@ -52,6 +78,22 @@ def parcel_similarity(model_name,plot=False):
         plt.imshow(w_cos_sim,vmin=-1,vmax=1)
 
     return w_cos_sim,cos_sim,kappa
+
+def agglomative_clustering(similarity):
+    # setting distance_threshold=0 ensures we compute the full tree.
+    model = AgglomerativeClustering(distance_threshold=0, 
+                                    n_clusters=None,
+                                    affinity='precomputed')
+
+    model = model.fit(X)
+    plt.title("Hierarchical Clustering Dendrogram")
+    # plot the top three levels of the dendrogram
+    plot_dendrogram(model, truncate_mode="level", p=3)
+    plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+    plt.show()
+
+
+
 
 
 if __name__ == "__main__":
