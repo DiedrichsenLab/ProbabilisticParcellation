c91ea6cd1effd2dd83422e4e4474ea643842c20b
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2022-11-02 19:06:25 -0400

    Update learn_fusion.py

--- learn_fusion.py
+++ learn_fusion.py
@@ -160,15 +160,16 @@ def batch_fit(datasets,sess,
     models=[]
     n_fits = n_rep
     info = pd.DataFrame({'name':[name]*n_fits,
-                         'atlas':[atlas.name]*n_fits,
-                         'K':[K]*n_fits,
-                         'datasets':[datasets]*n_fits,
-                         'sess':[sess]*n_fits,
-                         'type':[type]*n_fits,
-                         'subj':[subj]*n_fits,
-                         'arrange':[arrange]*n_fits,
-                         'emission':[emission]*n_fits,
-                         'loglik':[np.nan]*n_fits});
+                            'atlas':[atlas.name]*n_fits,
+                            'K':[K]*n_fits,
+                            'datasets':[datasets]*n_fits,
+                            'sess':[sess]*n_fits,
+                            'type':[type]*n_fits,
+                            'subj':[subj]*n_fits,
+                            'arrange':[arrange]*n_fits,
+                            'emission':[emission]*n_fits,
+                            'loglik':[np.nan]*n_fits,
+                            'weighting': [weighting]*n_fits});
 
 
     # Iterate over the number of fits
@@ -266,30 +267,11 @@ def fit_all(set_ind=[0,1,2,3],K=10,model_type='01',weighting=None):
     elif model_type=='02':
         uniform_kappa = False
 
-    elif model_type=='01-HCP02':
+    elif model_type[:6]=='01-HCP':
         uniform_kappa = True
-        weighting = np.repeat(0, len(set_ind)-1).tolist()
-        weighting.extend([0.2])
-
-    elif model_type=='01-HCP03':
-        uniform_kappa = True
-        weighting = np.repeat(0, len(set_ind)-1).tolist()
-        weighting.extend([0.3])
-
-    elif model_type=='01-HCP04':
-        uniform_kappa = True
-        weighting = np.repeat(0, len(set_ind)-1).tolist()
-        weighting.extend([0.4])
-
-    elif model_type=='01-HCP05':
-        uniform_kappa = True
-        weighting = np.repeat(0, len(set_ind)-1).tolist()
-        weighting.extend([0.5])
-
-    elif model_type=='01-HCP07':
-        uniform_kappa = True
-        weighting = np.repeat(0, len(set_ind)-1).tolist()
-        weighting.extend([0.7])
+        weighting = np.repeat(1, len(set_ind)-1).tolist()
+        hcp_weight = model_type.split('HCP')[1]
+        weighting.extend([float(f'{hcp_weight[0]}.{hcp_weight[1]}')])
 
     #Generate a dataname from first two letters of each training data set 
     dataname = [datasets[i][0:2] for i in set_ind]
