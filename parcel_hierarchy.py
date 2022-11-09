374022cac70d37bf9afce8879f11172abd4b1357
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2022-11-08 22:03:31 -0500

    reslice in SUIT

--- parcel_hierarchy.py
+++ parcel_hierarchy.py
@@ -300,15 +300,15 @@ def get_target(cmap):
     return tm,tl,tV
 
 def make_orthonormal(U):
-    """Gram-Schmidt process to make 
+    """Gram-Schmidt process to make
     matrix orthonormal"""
     n = U.shape[1]
     V=U.copy()
     for i in range(n):
         prev_basis = V[:,0:i]     # orthonormal basis before V[i]
-        rem = prev_basis @ prev_basis.T @ U[:,i]  
+        rem = prev_basis @ prev_basis.T @ U[:,i]
         # subtract projections of V[i] onto already determined basis V[0:i]
-        V[:,i] = U[:,i] - rem 
+        V[:,i] = U[:,i] - rem
         V[:,i] /= norm(V[:,i])
     return V
 
@@ -376,14 +376,14 @@ def colormap_mds(W,target=None,scale=False,clusters=None,gamma=0.3):
     return newcmp
 
 def export_map(data,atlas,cmap,labels,base_name):
-    """Exports a new atlas map as a Nifti (probseg), Nifti (desg), Gifti, and lut-file. 
+    """Exports a new atlas map as a Nifti (probseg), Nifti (desg), Gifti, and lut-file.
 
     Args:
         data (probabilities): Probabilstic parcellation to export
         atlas (): FunctionalFusion atlas type (SUIT2,MNISym3)
         cmap (): Colormap
         labels (list): List of labels for fields
-        base_name (_type_): File basename for atlas 
+        base_name (_type_): File basename for atlas
     """
     # Transform cmap into numpy array
     if not isinstance(cmap,np.ndarray):
@@ -394,8 +394,8 @@ def export_map(data,atlas,cmap,labels,base_name):
     probseg = suit_atlas.data_to_nifti(data)
     parcel = np.argmax(data,axis=0)+1
     dseg = suit_atlas.data_to_nifti(parcel)
-    
-    # Figure out correct mapping space 
+
+    # Figure out correct mapping space
     if atlas[0:4]=='SUIT':
         map_space='SUIT'
     elif atlas[0:7]=='MNISymC':
@@ -403,7 +403,7 @@ def export_map(data,atlas,cmap,labels,base_name):
     else:
         raise(NameError('Unknown atlas space'))
 
-    # Plotting label 
+    # Plotting label
     surf_data = suit.flatmap.vol_to_surf(probseg, stats='nanmean',
             space=map_space)
     surf_parcel = np.argmax(surf_data,axis=1)+1
@@ -432,12 +432,12 @@ def renormalize_probseg(probseg):
     X = probseg.get_fdata()
     xs = np.sum(X,axis=3)
     xs[xs==0]=np.nan
-    X = X/xs
+    X = X/np.expand_dims(xs,3)
     X[np.isnan(X)]=0
     probseg_img = nb.Nifti1Image(X,probseg.affine)
     parcel = np.argmax(X,axis=3)+1
     parcel[np.isnan(xs)]=0
-    dseg_img = nb.Nifti1Image(int(parcel),probseg.affine)
+    dseg_img = nb.Nifti1Image(parcel.astype(np.int8),probseg.affine)
     dseg_img.set_data_dtype('int8')
     dseg_img.header.set_intent(1002,(),"")
     dseg_img.header.set_slope_inter(1.0,0.0)
@@ -450,16 +450,16 @@ def resample_atlas(base_name):
     mnisym_dir=base_dir + '/Atlases/tpl-MNI152NLin2000cSymC'
     suit_dir=base_dir + '/Atlases/tpl-SUIT'
 
-    # Reslice to 1mm MNI and 1mm SUIT space 
+    # Reslice to 1mm MNI and 1mm SUIT space
     sym3 = nb.load(mnisym_dir + f'/{base_name}_space-MNISymC3_probseg.nii')
     tmp1 = nb.load(mnisym_dir + f'/tpl-MNISymC_res-1_gmcmask.nii')
     shap = tmp1.shape+sym3.shape[3:]
-    sym1 = ns.resample_from_to(sym3,(shap,tmp1.affine),1)
+    sym1 = ns.resample_from_to(sym3,(shap,tmp1.affine),3)
     sym1,dsym1= renormalize_probseg(sym1)
     nb.save(sym1,mnisym_dir + f'/{base_name}_space-MNISymC_probseg.nii')
     nb.save(dsym1,mnisym_dir + f'/{base_name}_space-MNISymC_dseg.nii')
-    
-    # Now put the image into SUIT space     
+
+    # Now put the image into SUIT space
     deform = nb.load(mnisym_dir + '/tpl-MNI152NLin2009cSymC_space-SUIT_xfm.nii')
     suit1 = nt.deform_image(sym1,deform,1)
     suit1,dsuit1= renormalize_probseg(sym1)
