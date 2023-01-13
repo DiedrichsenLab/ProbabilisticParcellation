c4653f6105fd85230c6015b961b50a34ca61e3f0
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-01-12 19:54:12 -0500

    Updated symmetric stats

--- scripts/evaluate_clustered.py
+++ scripts/evaluate_clustered.py
@@ -59,9 +59,9 @@ if not Path(base_dir).exists():
 atlas_dir = base_dir + f'/Atlases'
 res_dir = model_dir + f'/Results'
 
-def evaluate(K=10, symmetric='asym', model_type=None, model_name=None,
+def evaluate_task(K=10, symmetric='asym', model_type=None, model_name=None,
                   t_datasets=None, return_df=False, k_merged=None, load_best=True):
-    """Evaluate group and individual DCBC and coserr of all dataset fusion
+    """Evaluate group and individual DCBC and coserr of all task datasets fusion
        and any dataset training standalone on each of the datasets.
     Args:
         K: the number of parcels
@@ -116,9 +116,91 @@ def evaluate(K=10, symmetric='asym', model_type=None, model_name=None,
         results.to_csv(wdir + fname, index=False, sep='\t')
 
 
+def evaluate_rest(K=10, symmetric='asym', model_type=None, model_name=None,
+                  t_datasets=None, return_df=False, k_merged=None, load_best=True):
+    """Evaluate group and individual DCBC and coserr of all dataset fusion including rest
+       and any dataset training standalone on each of the datasets.
+    Args:
+        K: the number of parcels
+        t_datasets (list): a list of test datasets
+    Returns:
+        Write in evaluation file
+    """
+    # Preparing model type, name, and test set is not given
+    if model_type is None:
+        model_type = ['01','02','03','04','05']
+
+    if model_name is None:
+        model_name = ['Md','Po','Ni','Ib','Wm','De','So','Hc','MdPoNiIbWmDeSoHc']
+
+    if t_datasets is None:
+        t_datasets = ['MDTB','Pontine','Nishimoto','IBC',
+                      'WMFS','Demand','Somatotopic', 'HCP']
+
+    results = pd.DataFrame()
+    # Evaluate all single sessions on other datasets
+    m_name = []
+    for t in model_type:
+        print(f'- Start evaluating Model_{t} - {model_name}...')
+        m_name += [f'Models_{t}/{symmetric}_{nam}_space-MNISymC3_K-{K}' for nam in model_name]
+        if k_merged is not None:
+            m_name = [f'Models_{t}/{symmetric}_{nam}_space-MNISymC3_K-{K}_merged_K-{k_merged}' for nam in model_name]
+            load_best=False
+
+    for ds in t_datasets:
+        print(f'Testdata: {ds}\n')
+        # 1. Run DCBC individual
+        res_dcbc = run_dcbc_individual(m_name, ds, 'all', cond_ind=None,
+                                       part_ind='half', indivtrain_ind='half',
+                                       indivtrain_values=[1,2], device='cuda', load_best=load_best)
+        # 2. Run coserr individual
+        res_coserr = run_prederror(m_name, ds, 'all', cond_ind=None,
+                                   part_ind='half', eval_types=['group', 'floor'],
+                                   indivtrain_ind='half', indivtrain_values=[1,2],
+                                   device='cuda', load_best=load_best)
+        # 3. Merge the two dataframe
+        res = pd.merge(res_dcbc, res_coserr, how='outer')
+        results = pd.concat([results, res], ignore_index=True)
+
+    if return_df:
+        return results
+    else:
+        # Save file
+        wdir = model_dir + f'/Models/Evaluation'
+        fname = f'/eval_all_{symmetric}_K-{K}_datasetFusion.tsv'
+        if k_merged is not None:
+            fname = f'/eval_all_{symmetric}_K-{K}_merged_K-{k_merged}.tsv'
+        results.to_csv(wdir + fname, index=False, sep='\t')
+
+
 if __name__ == "__main__":
+
+    ############# Evaluate: Resting-state models #############
+    wdir = model_dir + f'/Models/Evaluation/sym'
+    T = pd.read_csv(base_dir + '/dataset_description.tsv', sep='\t')
+    D = pd.DataFrame()
+    datasets = [0, 1, 2, 3, 4, 5, 6, 7]
+    datanames = T.two_letter_code[datasets].to_list()
+    sym='sym'
+    Ks=[40, 68]
     
-    ############# Result 6: Clustered models #############
+
+    for i in range(8):        
+        for k in [22,26]:
+            res = evaluate_rest(K=K, symmetric=sym, model_type=['03'],
+                                model_name=['MdPoNiIbWmDeSoHc'], t_datasets=[T.name[i]],
+                                return_df=True)
+            
+            fname = f'/eval_{sym}_K-{K}_rest.tsv'
+            res.to_csv(wdir + fname, index=False, sep='\t')
+
+            D = pd.concat([D, res], ignore_index=True)
+    
+    fname = f'/eval_all8_{sym}_K-{K}.tsv'
+    D.to_csv(wdir + fname, index=False, sep='\t')
+    
+    
+    ############# Evaluate: Clustered models #############
     wdir = model_dir + f'/Models/Evaluation/sym'
     T = pd.read_csv(base_dir + '/dataset_description.tsv', sep='\t')
     D = pd.DataFrame()
@@ -129,10 +211,10 @@ if __name__ == "__main__":
     
 
     for i in range(7):        
-        for k_merged in [10,18,22,26]:
-            res = evaluate(K=K, symmetric=sym, model_type=['03'],
+        for k_merged in [22,26]:
+            res = evaluate_task(K=K, symmetric=sym, model_type=['03'],
                                 model_name=['MdPoNiIbWmDeSo'], t_datasets=[T.name[i]],
-                                return_df=True, k_merged=k_merged)
+                                return_df=True, k_merged=k_merged, load_best=False)
             
             fname = f'/eval_{sym}_K-{K}_merged_K-{k_merged}.tsv'
             res.to_csv(wdir + fname, index=False, sep='\t')
