d886756def66ef432f2e3178e6a69073635f98c6
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2023-01-15 14:23:06 -0500

    Parcel_hierarchy

--- parcel_hierarchy.py
+++ parcel_hierarchy.py
@@ -31,8 +31,7 @@ from matplotlib.colors import ListedColormap
 from matplotlib.patches import Rectangle
 from copy import deepcopy
 
-base_dir = '/Users/jdiedrichsen/Data/FunctionalFusion/'
-# base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion'
+base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion'
 if not Path(base_dir).exists():
     base_dir = '/srv/diedrichsen/data/FunctionalFusion'
 if not Path(base_dir).exists():
@@ -40,7 +39,7 @@ if not Path(base_dir).exists():
 if not Path(base_dir).exists():
     base_dir = '/Users/callithrix/Documents/Projects/Functional_Fusion/'
 if not Path(base_dir).exists():
-    base_dir = '/Users/jdiedrichsen/Data/Functional_Fusion/'
+    base_dir = '/Users/jdiedrichsen/Data/FunctionalFusion/'
 if not Path(base_dir).exists():
     raise(NameError('Could not find base_dir'))
 
@@ -524,67 +523,6 @@ def make_asymmetry_map(mname, cmap='hot', cscale=[0.3,1]):
     # ax.show()
     return sym_score
 
-
-def analyze_parcel(mname, load_best=True, sym=True):
-
-    fileparts = mname.split('/')
-    split_mn = fileparts[-1].split('_')
-    if load_best:
-        info,model = load_batch_best(mname)
-    else:
-        info,model = load_batch_fit(mname)
-
-    # model.move_to('cpu')
-
-    # Get parcel similarity:
-    w_cos_sym,_,_ = parcel_similarity(model,plot=True,sym=sym)
-
-    # groups=['I','L','W','A','O','D','M']
-    # Do Clustering:
-    num_clusters = 7
-    if num_clusters > model.arrange.K:
-        num_clusters = model.arrange.K-1
-    labels,clusters,leaves = agglomative_clustering(w_cos_sym,sym=sym,num_clusters=4,plot=False)
-    ax = plt.gca()
-
-    # Make a colormap
-    w_cos_sim,_,_ = parcel_similarity(model,plot=False)
-    W = calc_mds(w_cos_sim,center=True)
-
-    # Define color anchors
-    m = np.array([0.65,0.65,0.65])
-
-
-
-    # Desired orientation of the eigenvectors of MDS in colorspace
-    V=np.array([[-0.3,-0.6,1],[1,-.6,-.7],[1,1,1]]).T
-    V=make_orthonormal(V)
-
-    cmap = colormap_mds(W,target=(m,V),clusters=clusters,gamma=0)
-
-    # Replot the Clustering dendrogram, this time with the correct color map
-    agglomative_clustering(w_cos_sym,sym=sym,num_clusters=7,plot=True,cmap=cmap)
-    plot_colormap(cmap(np.arange(model.K)))
-
-    # Plot the parcellation
-    Prob = np.array(model.marginal_prob())
-    parcel = Prob.argmax(axis=0)+1
-    atlas = split_mn[2][6:]
-    ax = plot_data_flat(parcel,atlas,cmap = cmap,
-                    dtype='label',
-                    labels=labels,
-                    render='plotly')
-    ax.show()
-
-    return Prob,parcel,atlas,labels,cmap
-
-def make_sfn_atlas():
-    Prob,parcel,atlas,labels,cmap = analyze_parcel(mname,sym=True)
-    # Quick hack - hard-code the labels:
-    labels = np.array(['0', 'O1L', 'W1L', 'A2L', 'A3L', 'L1L', 'O2L', 'D1L', 'L2L', 'M2L','I1L', 'D2L', 'M3L', 'M4L', 'M1L', 'W4L', 'A1L', 'W2L', 'O1R', 'W1R', 'A2R', 'A3R', 'L1R', 'O2R', 'D1R', 'L2R', 'M2R', 'I1R', 'D2R', 'M3R', 'M4R', 'M1R', 'W4R', 'A1R', 'W2R'], dtype=object)
-    export_map(Prob,atlas,cmap,labels,base_dir + '/Atlases/tpl-MNI152NLin2000cSymC/atl-NettekovenSym34')
-    resample_atlas('atl-NettekovenSym34')
-
 def map_fine2coarse(fine_probabilities, coarse_probabilities):
     """Maps parcels of a fine parcellation to parcels of a coarse parcellation.
 
@@ -735,6 +673,64 @@ def get_clustered_model(mname_fine, mname_coarse, sym=True, reduce=True):
     return merged_model, mname_merged
 
 
+def analyze_parcel(mname, load_best=True, sym=True):
+
+    fileparts = mname.split('/')
+    split_mn = fileparts[-1].split('_')
+    if load_best:
+        info,model = load_batch_best(mname)
+    else:
+        info,model = load_batch_fit(mname)
+
+    # Get parcel similarity:
+    w_cos_sym,_,_ = parcel_similarity(model,plot=True,sym=sym)
+
+    # groups=['I','L','W','A','O','D','M']
+    # Do Clustering:
+    num_clusters = 7
+    if num_clusters > model.arrange.K:
+        num_clusters = model.arrange.K-1
+    labels,clusters,leaves = agglomative_clustering(w_cos_sym,sym=sym,num_clusters=4,plot=False)
+    ax = plt.gca()
+
+    # Make a colormap
+    w_cos_sim,_,_ = parcel_similarity(model,plot=False)
+    W = calc_mds(w_cos_sim,center=True)
+
+    # Define color anchors
+    m = np.array([0.65,0.65,0.65])
+
+    # Desired orientation of the eigenvectors of MDS in colorspace
+    V=np.array([[-0.3,-0.6,1],[1,-.6,-.7],[1,1,1]]).T
+    V=make_orthonormal(V)
+
+    cmap = colormap_mds(W,target=(m,V),clusters=clusters,gamma=0)
+
+    # Replot the Clustering dendrogram, this time with the correct color map
+    agglomative_clustering(w_cos_sym,sym=sym,num_clusters=7,plot=True,cmap=cmap)
+    plot_colormap(cmap(np.arange(model.K)))
+
+    # Plot the parcellation
+    Prob = np.array(model.marginal_prob())
+    parcel = Prob.argmax(axis=0)+1
+    atlas = split_mn[2][6:]
+    ax = plot_data_flat(parcel,atlas,cmap = cmap,
+                    dtype='label',
+                    labels=labels,
+                    render='plotly')
+    ax.show()
+
+    return Prob,parcel,atlas,labels,cmap
+
+def make_sfn_atlas():
+    Prob,parcel,atlas,labels,cmap = analyze_parcel(mname,sym=True)
+    # Quick hack - hard-code the labels:
+    labels = np.array(['0', 'O1L', 'W1L', 'A2L', 'A3L', 'L1L', 'O2L', 'D1L', 'L2L', 'M2L','I1L', 'D2L', 'M3L', 'M4L', 'M1L', 'W4L', 'A1L', 'W2L', 'O1R', 'W1R', 'A2R', 'A3R', 'L1R', 'O2R', 'D1R', 'L2R', 'M2R', 'I1R', 'D2R', 'M3R', 'M4R', 'M1R', 'W4R', 'A1R', 'W2R'], dtype=object)
+    export_map(Prob,atlas,cmap,labels,base_dir + '/Atlases/tpl-MNI152NLin2000cSymC/atl-NettekovenSym34')
+    resample_atlas('atl-NettekovenSym34')
+
+
+
 def merge_clusters():
     save_dir = '/Users/callithrix/Documents/Projects/Functional_Fusion/Models/'
     # --- Merge parcels at K=20, 34 & 40 ---
@@ -752,7 +748,8 @@ def merge_clusters():
 
 
 if __name__ == "__main__":
-    mname = 'Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC2_K-20'
+    
+    mname = 'Models_03/sym_MdPo_space-MNISymC3_K-40'
     Prob,parcel,atlas,labels,cmap = analyze_parcel(mname,sym=True)
     pass
 
