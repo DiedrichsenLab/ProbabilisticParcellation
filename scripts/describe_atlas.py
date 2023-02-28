3a598dbfd49e39487914edb05bb0b0823a15e22b
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-02-28 00:51:51 -0500

    Update describe_atlas.py

--- scripts/describe_atlas.py
+++ scripts/describe_atlas.py
@@ -27,22 +27,37 @@ import logging
 pt.set_default_tensor_type(pt.FloatTensor)
 
 
-def correlate_profile(data, profile):
-
-    pass
+def correlate(X, Y):
+    """ Correlate X and Y numpy arrays after standardizing them"""
+    X = util.zstandarize_ts(X)
+    Y = util.zstandarize_ts(Y)
+    return Y.T @ X / X.shape[0]
 
 
 def get_cortex(method='corr', mname='Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC2_K-32_meth-mixed'):
+
+    # Get the model and parcel profile
     info, model = ut.load_batch_best(mname)
     info = fp.recover_info(info, model, mname)
-    profile_file = f'{ut.model_dir}/Atlases/{mname.split("/")[-1]}_task_profile_data.tsv'
+    lut_file = ut.model_dir + '/Atlases/' + mname.split('/')[-1] + '.lut'
+    if Path(lut_file).exists():
+        index, cmap, labels = nt.read_lut(lut_file)
+    profile_file = f'{ut.model_dir}/Atlases/{mname.split("/")[-1]}_profile.tsv'
     if Path(profile_file).exists():
         parcel_profiles = pd.read_csv(
             profile_file, sep="\t"
         )
     else:
+        parcel_profiles, profile_data = fp.get_profiles(model, info)
 
-        parcel_profiles, profile_data = fp.get_profile(model, info)
+    # Make profile into numpy array
+    if isinstance(parcel_profiles, pd.DataFrame):
+        idx_start = parcel_profiles.columns.tolist().index('condition') + 1
+        parcel_names = parcel_profiles.columns[idx_start:idx_start + info.K]
+        profile = parcel_profiles.iloc[:, idx_start:idx_start + info.K]
+        profile = profile.values
+
+    # Get cortical data
     dat = []
     for d, dataset in enumerate(info.datasets):
 
@@ -56,8 +71,21 @@ def get_cortex(method='corr', mname='Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC2
         dat.append(Davg)
     data = np.concatenate(dat, axis=0)
 
-    cortex = correlate_profile(data, parcel_profiles)
-    return cortex
+    # Correlat parcel profiles with cortical data
+    cortex = correlate(data, profile)
+
+    # Get the fs32k atlas
+    atlas, _ = am.get_atlas('fs32k', dataset.atlas_dir)
+
+    # Save the data
+    if labels is None:
+        labels = parcel_names
+    else:
+        labels = labels[1:]
+
+    C = atlas.data_to_cifti(cortex, labels)
+    nb.save(
+        C, f'{ut.model_dir}/Atlases/{mname.split("/")[-1]}_cortex-{method}.dscalar.nii')
 
 
 if __name__ == "__main__":
