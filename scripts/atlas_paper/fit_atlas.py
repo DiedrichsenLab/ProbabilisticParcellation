3ae00ca696b263e325725a3f75ef7a36f260b774
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-05-18 18:27:06 -0400

    Update fit_atlas.py

--- scripts/atlas_paper/fit_atlas.py
+++ scripts/atlas_paper/fit_atlas.py
@@ -62,25 +62,22 @@ def fit_asym_from_sym_sep_hem(mname='Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC3
     return M, new_info
 
 
-def fit_asym_from_sym(mname='Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC3_K-68'):
-    # Load model
-    inf, m = ut.load_batch_best(mname)
-    inf = ut.recover_info(inf, m, mname)
-    # Freeze emission model and fit arrangement model
-    M, new_info = lf.refit_model(m, inf, fit='arrangement', sym_new='asym')
-    # save new model
-    mname_new = f'asym_{mname.split("sym_")[1]}_arrange-asym'
-    with open(f'{ut.model_dir}/Models/{mname_new}.pickle', 'wb') as file:
-        pickle.dump([M], file)
-        # save new info
-        new_info.to_csv(f'{ut.model_dir}/Models/{mname_new}.tsv',
-                        sep='\t', index=False)
-        print(
-            f'Done. Saved asymmetric model as: \n\t{mname_new} \nOutput folder: \n\t{ut.model_dir}/Models/ \n\n')
-    return M, new_info
+def fit_models(ks, fit_datasets=['all', 'loo', 'indiv'], rest_included=False, verbose=True, indiv_on_rest_only=False, sym_from_asym=False, mname_asym=None):
+    """
+    Fits models with different parameters and datasets.
 
+    Args:
+        ks (list): A list of values for the parameter K.
+        fit_datasets (list): A list of strings specifying which datasets to fit the models on. 
+                             Possible values: 'all', 'loo', 'indiv'. Default is ['all', 'loo', 'indiv'].
+        rest_included (bool): Flag indicating whether the HCP dataset is included in the fit. Default is False.
+        verbose (bool): Flag indicating whether to print verbose output. Default is True.
+        indiv_on_rest_only (bool): Flag indicating whether to fit individual datasets on the rest dataset only. 
+                                   Default is False.
+        sym_from_asym (bool): Flag indicating whether to fit symmetric models from asymmetric models. Default is False.
+        mname_asym (str): The name of the asymmetric model. Required when sym_from_asym is True.
 
-def fit_models(ks, fit_datasets=['all', 'loo', 'indiv'], rest_included=False, verbose=True, indiv_on_rest_only=False):
+    """
 
     ########## Settings ##########
     space = 'MNISymC3'  # Set atlas space
@@ -115,16 +112,26 @@ def fit_models(ks, fit_datasets=['all', 'loo', 'indiv'], rest_included=False, ve
             wdir = ut.model_dir + f'/Models/Models_{t}'
             fname = f'/{msym}_{datanames}_space-{space}_K-{k}.tsv'
 
-            if not Path(wdir + fname).exists():
-                print(
-                    f'fitting model {t} with K={k} in space {space} as {fname}...')
-                if verbose:
-                    ut.report_cuda_memory()
-                lf.fit_all(datasets, k, model_type=t, repeats=100,
-                           sym_type=[msym], space=space)
+            if not sym_from_asym:
+                if not Path(wdir + fname).exists():
+                    print(
+                        f'fitting model {t} with K={k} in space {space} as {fname}...')
+                    if verbose:
+                        ut.report_cuda_memory()
+                    lf.fit_all(datasets, k, model_type=t, repeats=100,
+                               sym_type=[msym], space=space)
+                else:
+                    print(
+                        f'model {t} with K={k} in space {space} already fitted as {fname}')
             else:
-                print(
-                    f'model {t} with K={k} in space {space} already fitted as {fname}')
+                mname = f'Models_{t}/sym_{datanames}_space-{space}_K-{k}'
+                mname_asym = f'Models_{t}/asym_{datanames}_space-{space}_K-{k}_arrange-asym_sep-hem'
+                if not Path(wdir + f'/{mname_asym}.tsv').exists():
+                    fit_asym_from_sym_sep_hem(
+                        mname=mname, mname_new=mname_asym)
+                else:
+                    print(
+                        f'model {t} with K={k} in space {space} already fitted as {mname_asym}')
 
 
 if __name__ == "__main__":
@@ -136,12 +143,12 @@ if __name__ == "__main__":
     #    rest_included=True, indiv_on_rest_only=True)
     # fit_models(ks=ks, fit_datasets=['loo'], rest_included=True)
 
-    # fit_asym_from_sym(
-    # mname='Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC2_K-68')
     # fit_asym_from_sym_sep_hem(
     #     mname='Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC2_K-68', mname_new='Models_03/asym_MdPoNiIbWmDeSo_space-MNISymC2_K-68_arrange-asym_sep-hem')
 
-    ks = [10, 20, 34, 40, 68, 14, 28, 32, 56, 60]
-    for k in ks:
-        fit_asym_from_sym_sep_hem(
-            mname=f'Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC2_K-{k}', mname_new=f'Models_03/asym_MdPoNiIbWmDeSo_space-MNISymC2_K-{k}_arrange-asym_sep-hem')
+    atlas = 'MNISymC3'
+    ks = [10, 20, 34, 40, 68]
+    fit_models(ks=ks, fit_datasets=['indiv', 'loo', 'all'],
+               rest_included=False, indiv_on_rest_only=False, sym_from_asym=True)
+
+    ks_additional = [14, 28, 32, 56, 60]
