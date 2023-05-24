2b84688c8ec999a7290832501e458cfc4ea7cbe0
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2023-05-24 16:04:55 -0400

    update_colormap

--- scripts/atlas_paper/describe_atlas.py
+++ scripts/atlas_paper/describe_atlas.py
@@ -147,6 +147,15 @@ def profile_NettekovenSym68c32():
     fp.export_profile(mname_new, info, model, labels)
     features = fp.cognitive_features(mname_new)
 
+def update_color_map(mname,):
+    atlas_dir = '/Volumes/diedrichsen_data$/data/Cerebellum/ProbabilisticParcellationModel/Atlases/'
+    _, cmap, labels = nt.read_lut(atlas_dir + mname + '.lut')
+    Prob, parcel, atlas, labels, cmap = ea.colour_parcel(
+        mname='Models_03/' + mname , labels=labels)
+    cmap_array = np.array(cmap(np.arange(len(labels))))
+    save_lut(base_name + '.lut',
+                np.arange(len(labels)), cmap[:, 0:4], labels)
+
 
 def export_model_merged(mname_new):
     space = mname_new.split('space-')[1].split('_')[0]
@@ -181,8 +190,8 @@ def save_pmaps(Prob, labels, atlas, subset=[0, 1, 2, 3, 4, 5]):
 
 if __name__ == "__main__":
 
-    # mname = 'Models_03/sym_MdPoNiIbWmDeSo_space-MNISymC2_K-32_meth-mixed'
-
+    mname = 'sym_MdPoNiIbWmDeSo_space-MNISymC2_K-68_reordered'
+    update_color_map(mname)
     # # --- Export merged model profile ---
     # fileparts = mname.split('/')
     # split_mn = fileparts[-1].split('_')
