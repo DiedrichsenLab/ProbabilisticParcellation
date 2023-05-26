59884bcdd429b41cc893bf5f1b98b315aedc14b1
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-05-26 12:32:49 -0400

    Added asym

--- scripts/atlas_paper/parcel_hierarchy.py
+++ scripts/atlas_paper/parcel_hierarchy.py
@@ -21,7 +21,7 @@ import ProbabilisticParcellation.similarity_colormap as sc
 import ProbabilisticParcellation.export_atlas as ea
 import ProbabilisticParcellation.functional_profiles as fp
 import Functional_Fusion.dataset as ds
-import HierarchBayesParcel.evaluation as ev
+import generativeMRF.evaluation as ev
 
 pt.set_default_tensor_type(pt.FloatTensor)
 
