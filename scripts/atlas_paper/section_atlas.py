b5d356c4c21d7bd0bdf5ea9a9f0cfefc82e59b20
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-07-09 00:55:47 -0400

    Update section_atlas.py

--- scripts/atlas_paper/section_atlas.py
+++ scripts/atlas_paper/section_atlas.py
@@ -42,12 +42,127 @@ atlas_dir = (
 )
 suit_dir = "/Volumes/diedrichsen_data$/data/FunctionalFusion/Atlases/tpl-SUIT/"
 
+def size_plot(Prob, cmap, labels):
+    # Plot parcel size for left parcels
+    if 'superior' in labels[1]:
+        plt.figure(figsize=(10, 40))
+        D = cl.plot_parcel_size(Prob, ListedColormap(cmap), labels, wta=True, sort=False, side='L')
+        plt.savefig(ut.figure_dir + f"parcel_sizes_section.pdf")
+        
+        plt.figure(figsize=(10, 40))
+        D = cl.plot_parcel_size(Prob, ListedColormap(cmap), labels, wta=False, sort=False, side='L')
+        plt.savefig(ut.figure_dir + f"parcel_sizes_section_probs.pdf")
+        
+        if 'vermis' in labels[4]:
+            plt.figure(figsize=(10, 40))
+            D = cl.plot_parcel_size(Prob, ListedColormap(cmap), labels, wta=True, sort=False, side='L')
+            plt.savefig(ut.figure_dir + f"parcel_sizes_section_vermis.pdf")
+            
+            plt.figure(figsize=(10, 40))
+            D = cl.plot_parcel_size(Prob, ListedColormap(cmap), labels, wta=False, sort=False, side='L')
+            plt.savefig(ut.figure_dir + f"parcel_sizes_section_probs_vermis.pdf")
+
+    else:
+        plt.figure()
+        D = cl.plot_parcel_size(Prob, ListedColormap(cmap), labels, wta=True, sort=False, side='L')
+        plt.savefig(ut.figure_dir + f"parcel_sizes.pdf")
+
+        plt.figure()
+        D = cl.plot_parcel_size(Prob, ListedColormap(cmap), labels, wta=False, sort=False, side='L')
+        plt.savefig(ut.figure_dir + f"parcel_sizes_probs.pdf")
+
+def import_lobules(space):
+    # Get lobules 
+    a, ainf = am.get_atlas(space, ut.atlas_dir)
+    
+    lobule_file =  "atl-Anatom_space-SUIT_dseg.nii"
+    lobules = a.read_data(suit_dir + lobule_file, 0)
+
+    # Get lobule labels
+    _, _, lobule_labels = nt.read_lut(suit_dir + f"{lobule_file.split('_space-')[0]}.lut")
+    return lobules, lobule_labels
 
 
+def get_sections(lobules, lobule_labels, vermis=False):
+    # Primary section is in lobule 1 - 7a (superior to horizontal fissure)
+    # Secondary section is in lobule 7a - 8 (between horizontal fissure and secondary fissure)
+    # Tertiary section is in lobule 9 and 10 (inferior to secondary fissure)
+
+    boundaries = ["Left_I_IV", "Left_CrusII", "Left_IX", "Left_Dentate"]
+    lobule_boundaries = [lobule_labels.index(b) for b in boundaries]
+
+    # make empty mask with each section as a row
+    mask = np.zeros((lobules.shape[0]))
+    prim = (lobules > lobule_boundaries[0]) & (lobules <= lobule_boundaries[1])
+    sec = (lobules > lobule_boundaries[1]) & (lobules <= lobule_boundaries[2])
+    tert = (lobules > lobule_boundaries[2]) & (lobules <= lobule_boundaries[3])
+    mask[prim] = 1
+    mask[sec] = 2
+    mask[tert] = 3
+
+    if vermis:
+        # Section out the vermis
+        vermis_indices = [idx+1 for idx, label in enumerate(lobule_labels) if 'Vermis' in label]  
+        prim_vermis = prim & (np.isin(lobules, vermis_indices))
+        sec_vermis = sec & (np.isin(lobules, vermis_indices))
+        tert_vermis = tert & (np.isin(lobules, vermis_indices))
+        mask[prim_vermis] = 4
+        mask[sec_vermis] = 5
+        mask[tert_vermis] = 6
+    
+    # Plot mask   
+    plt.figure()
+    ut.plot_data_flat(mask.astype(int), 'MNISymC2',
+                   dtype='label',
+                   render='matplotlib',
+                   bordersize=4,
+                   cmap='Set1',
+                   bordercolor='k',
+                   backgroundcolor='w')
+    if vermis:
+        plt.savefig(ut.figure_dir + f"lobule_sections_vermis.png")
+    else:
+        plt.savefig(ut.figure_dir + f"lobule_sections.png")
+
+    # Get indicator matrix for primary, secondary and tertiary parcels
+    indicator_mask = matrix.indicator(mask)
+    # Remove parcels where there is no lobule data (first column)
+    indicator_mask = indicator_mask[:,1:]
+    
+    return mask, indicator_mask
+
+
+def divide_parcels(Prob, indicator_mask, cmap, vermis=False):
+    # Get sections by multiplying the indicator matrix with the Prob matrix, so that I have the probabilities for each parcel split into sections
+    # Multiply a matrix with a vector without summing over the columns
+    prim_prob = indicator_mask[:,0].T * Prob
+    sec_prob = indicator_mask[:,1].T * Prob
+    tert_prob = indicator_mask[:,2].T * Prob
+    sections = ['superior', 'dorsal', 'inferior']
+    if vermis:
+        sections.extend(['vermis_superior', 'vermis_dorsal', 'vermis_inferior'])
+        prim_verm_prob = indicator_mask[:,3].T * Prob
+        sec_verm_prob = indicator_mask[:,4].T * Prob
+        tert_verm_prob = indicator_mask[:,5].T * Prob
+        zip_sections = zip(prim_prob, sec_prob, tert_prob, prim_verm_prob, sec_verm_prob, tert_verm_prob)
+        n_sections = 6
+    else:
+        zip_sections = zip(prim_prob, sec_prob, tert_prob)
+        n_sections = 3
+    Prob_sections = np.vstack(zip_sections)
+    cmap_sections = np.vstack((cmap[0], np.repeat(cmap[1:], n_sections, axis=0)))
+
+    # Sort parcels that start with M labels first, then A, D, S
+    domains = ['M', 'A', 'D', 'S']
+    regions = np.arange(1,5)
+    hemispheres = ['L', 'R']
+    labels_sections = ['0'] + [f"{d}{r}_{s}_{h}" for h in hemispheres for d in domains  for r in regions  for s in sections]
+
+    return Prob_sections, cmap_sections, labels_sections
 
 def dissect_atlas(mname):
     '''Dissect the atlas into primary, secondary and tertiary parcels'''
-    # Import probabilities and parcels
+    # --- Import probabilities and parcels ---
     fileparts = mname.split("/")
     space = mname.split("_space-")[1]
     atlasname = fileparts[1].split("_space")[0]
@@ -63,94 +178,21 @@ def dissect_atlas(mname):
     
     _, cmap, labels = nt.read_lut(atlas_dir + atlasname + ".lut")
     
+    size_plot(Prob, cmap, labels)
+
+    # --- Get lobules ---
+    lobules, lobule_labels = import_lobules(space)  
+
+    # --- Get sections ---
+    _, indicator_mask = get_sections(lobules, lobule_labels, vermis=True)
 
     
+    # --- Divide parcels into sections ---
+    Prob_sections, cmap_sections, labels_sections = divide_parcels(Prob, indicator_mask, cmap, vermis=True)
     
+    # --- Plot section sizes ---
+    size_plot(Prob_sections, cmap_sections, labels_sections)
 
-    # Plot parcel size for left parcels
-    D = cl.plot_parcel_size(Prob, ListedColormap(cmap), labels, wta=True, sort=False, side='L')
-    plt.savefig(ut.figure_dir + f"parcel_sizes.pdf")
-
-    D = cl.plot_parcel_size(Prob, ListedColormap(cmap), labels, wta=False, sort=False, side='L')
-    plt.savefig(ut.figure_dir + f"parcel_sizes_probs.pdf")
-
-
-    
-    # Get lobules 
-    a, ainf = am.get_atlas(space, ut.atlas_dir)
-    
-    lobule_file =  "atl-Anatom_space-SUIT_dseg.nii"
-    lobules = a.read_data(suit_dir + lobule_file, 0)
-
-    # Get lobule labels
-    _, _, lobule_labels = nt.read_lut(suit_dir + f"{lobule_file.split('_space-')[0]}.lut")
-
-    # Primary section is in lobule 1 - 7a (superior to horizontal fissure)
-    # Secondary section is in lobule 7a - 8 (between horizontal fissure and secondary fissure)
-    # Tertiary section is in lobule 9 and 10 (inferior to secondary fissure)
-    boundaries = ["Left_I_IV", "Left_CrusII", "Left_IX", "Left_Dentate"]
-    # boundaries = ["Left_I_IV", "Right_CrusI", "Left_CrusII", "Right_VIIIb", "Left_IX", "Vermis_IX"]
-    lobule_boundaries = [lobule_labels.index(b) for b in boundaries]
-    # make empty mask with each section as a row
-    mask = np.zeros((lobules.shape[0]))
-    prim = (lobules > lobule_boundaries[0]) & (lobules <= lobule_boundaries[1])
-    sec = (lobules > lobule_boundaries[1]) & (lobules <= lobule_boundaries[2])
-    tert = (lobules > lobule_boundaries[2]) & (lobules <= lobule_boundaries[3])
-    mask[prim] = 1
-    mask[sec] = 2
-    mask[tert] = 3
-    
-    # Plot mask   
-    ut.plot_data_flat(mask.astype(int), 'MNISymC2',
-                   dtype='label',
-                   render='matplotlib',
-                   bordersize=4,
-                   cmap='Set1',
-                   bordercolor='k',
-                   backgroundcolor='w')
-    plt.savefig(ut.figure_dir + f"lobule_sections.png")
-
-    
-    # Get indicator matrix for primary, secondary and tertiary parcels
-    indicator_mask = matrix.indicator(mask)
-    # Remove parcels where there is no lobule data (first column)
-    indicator_mask = indicator_mask[:,1:]
-
-    # Get sections by multiplying the indicator matrix with the Prob matrix, so that I have the probabilities for each parcel split into sections
-    # Multiply a matrix with a vector without summing over the columns
-    prim_prob = indicator_mask[:,0].T * Prob
-    sec_prob = indicator_mask[:,1].T * Prob
-    tert_prob = indicator_mask[:,2].T * Prob
-    # Zip the sections into one matrix
-    zip_sections = zip(prim_prob, sec_prob, tert_prob)
-    Prob_sections = np.vstack(zip_sections)
-    cmap_sections = np.vstack((cmap[0], np.repeat(cmap[1:], 3, axis=0)))
-
-    # Sort parcels that start with M labels first, then A, D, S
-    domains = ['M', 'A', 'D', 'S']
-    regions = np.arange(1,5)
-    sections = ['superior', 'dorsal', 'inferior']
-    hemispheres = ['L', 'R']
-    labels_sections = ['0'] + [f"{d}{r}_{s}_{h}" for h in hemispheres for d in domains  for r in regions  for s in sections]
-
-    
-    # Plot parcel size for left parcels
-    D = cl.plot_parcel_size(Prob_sections, ListedColormap(cmap_sections), labels_sections, wta=True, sort=False, side='L')
-    # Adjust figure size
-    plt.gcf().set_size_inches(10, 40)
-    plt.savefig(ut.figure_dir + f"parcel_sizes_SDI.pdf")
-
-    D = cl.plot_parcel_size(Prob_sections, ListedColormap(cmap_sections), labels_sections, wta=False, sort=False, side='L')
-    # Adjust figure size
-    plt.gcf().set_size_inches(10, 40)
-    plt.savefig(ut.figure_dir + f"parcel_sizes_SDI_probs.pdf")
-
-    
-    
-    
-
-
-    pass
 
 
 
