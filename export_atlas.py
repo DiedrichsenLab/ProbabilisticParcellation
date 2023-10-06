51ed8fd810cb6dd517dc02b20431b8776d5cb7d6
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2023-10-06 13:58:24 -0400

    Export Atlas Spatial

--- export_atlas.py
+++ export_atlas.py
@@ -25,6 +25,144 @@ import ProbabilisticParcellation.plot as ppp
 conn_dir = '/Volumes/diedrichsen_data$/data/Cerebellum/connectivity/maps/'
 surf_dir = surf.plot._surf_dir
 
+def renormalize_probseg(probseg, mask):
+    """Renormalizes a probsegmentation file
+    after resampling, so that the probabilies add up to 1
+
+    Args:
+        probseg (nifti_img):
+
+    Returns:
+        probseg_img (NiftiImage): renormalize Prob segmentation
+        dseg_img (NiftiImage): desementation file
+    """
+    X = probseg.get_fdata()
+    xs = np.sum(X, axis=3)
+    X = X / np.expand_dims(xs, 3)
+    maskX = mask.get_fdata()
+    X[maskX == 0] = np.nan
+    probseg_img = nb.Nifti1Image(X, probseg.affine)
+    parcel = np.argmax(X, axis=3) + 1
+    parcel[maskX == 0] = 0
+    dseg_img = nb.Nifti1Image(parcel.astype(np.uint8), probseg.affine)
+    dseg_img.set_data_dtype("uint8")
+    dseg_img.header.set_intent(1002,(),"")
+    probseg_img.set_data_dtype("float32")
+    # probseg_img.header.set_slope_inter(1/(2**16-1),0.0)
+    return probseg_img, dseg_img
+
+def resample_atlas(fname, atlas="MNISymC2", target_space="MNI152NLin2009cSymC"):
+    """ Resamples probabilistic atlas from MNISymC2 to a new atlas space in 1mm resolution
+    Args:
+        fname (str): Name of the atlas
+        atlas (str/atlas): FunctionalFusion atlas (SUIT2,MNISym3, fs32k)
+        target_space (str): Target space (MNI152NLin2009cSymC, MNI152NLin2009cAsym)
+    """
+    a, ainf = am.get_atlas(atlas, ut.atlas_dir)
+    src_dir = ut.model_dir + "/Atlases/"
+    targ_dir = ut.base_dir + f"/Atlases/tpl-{target_space}"
+    # Load and set NaNs to 0
+    nii_atlas = nb.load(src_dir + f"/{fname}_space-{atlas}_probseg.nii")
+    X = np.nan_to_num(nii_atlas.get_fdata())
+    nii_atlasf = nb.Nifti1Image(X, nii_atlas.affine, nii_atlas.header)
+    # Reslice to 1mm MNI
+    print("normalizing")
+    if ainf["space"] != target_space:
+        print(f"deforming from {ainf['space']} to {target_space}")
+        deform = nb.load(
+            targ_dir + f"/tpl-{target_space}_from-{ainf['space']}_mode-image_xfm.nii"
+        )
+        nii_res = nt.deform_image(nii_atlasf, deform, 1)
+        # Get target space mask:
+        mname = f"tpl-{target_space}_res-1_gmcmask.nii"
+        nii_mask = nb.load(targ_dir + "/" + mname)
+    else:
+        # Make new shape
+        mname = ainf["mask"]
+        mname = mname.replace("res-2", "res-1")
+        nii_mask = nb.load(targ_dir + "/" + mname)
+        shap = nii_mask.shape + nii_atlas.shape[3:]
+        nii_res = ns.resample_from_to(nii_atlasf, (shap, nii_mask.affine), 1)
+    nii, dnii = renormalize_probseg(nii_res, nii_mask)
+    print("saving")
+    nb.save(nii, targ_dir + f"/atl-{fname}_space-{target_space}_probseg.nii")
+    nb.save(dnii, targ_dir + f"/atl-{fname}_space-{target_space}_dseg.nii")
+
+
+def prob_to_label_gii(probseg,atlas_space,cmap,label_names):
+    """Generates a surface gifti file with the correct labels
+    and colors from a probabilistic parcellation (nifti1Image)
+
+    Args:
+        probseg (Nifti1Image): _description_
+        mapspace (str): 'SUIT' or 'MNISymC'
+        cmap (nd.array): Color map including a row for 0 
+        label_names (list): label names (including a ??? for 0)
+    """
+    # Error if cmap and label_names are not the same length
+    if cmap.shape[0] != len(label_names):
+        raise (NameError("cmap and label_names must be the same length"))
+
+    # Add the alpha column if it is missing
+    if cmap.shape[1] ==3:
+        cmap = np.concatenate((cmap,np.ones((cmap.shape[0],1))),axis=1)
+
+    # Figure out correct mapping space
+    if atlas_space.startswith("SUIT"):
+        map_space = "SUIT"
+    elif atlas_space.startswith("MNISymC"):
+        map_space = "MNISymC"
+    elif atlas_space == "MNI152NLin2009cSymC":
+        map_space = "MNISymC"
+    else:
+        raise (NameError("Unknown atlas space"))
+
+    # Plotting label
+    surf_data = suit.flatmap.vol_to_surf(probseg, 
+                                         stats="nanmean", 
+                                         space=map_space)
+    surf_parcel = np.argmax(surf_data, axis=1) + 1
+    gifti = nt.make_label_gifti(
+        surf_parcel.reshape(-1, 1),
+        anatomical_struct="Cerebellum",
+        labels=np.arange(probseg.shape[3] + 1),
+        label_names=label_names,
+        label_RGBA=cmap)
+    return gifti
+
+
+def export_map(data, atlas, cmap, label_names, base_name):
+    """Exports a marginal probability of a arrangement model to a Nifti (probseg), Nifti (dseg), Gifti, and lut-file.
+
+    Args:
+        data (probabilities): Marginal probabilities of the arrangement model
+        atlas (str/atlas): FunctionalFusion atlas (SUIT2,MNISym3, fs32k)
+        cmap (ListedColormap): Colormap
+        labels (list): List of labels for fields
+        base_name (_type_): File directory + basename for atlas
+    """
+    # Error if cmap and label_names are not the same length
+    if cmap.shape[0] != len(label_names):
+        raise (NameError("cmap and label_names must be the same length"))
+    # Transform cmap into numpy array
+    if not isinstance(cmap, np.ndarray):
+        cmap = cmap(np.arange(cmap.N))
+
+    suit_atlas, _ = am.get_atlas(atlas, ut.base_dir + "/Atlases")
+    probseg = suit_atlas.data_to_nifti(data)
+    parcel = np.argmax(data, axis=0) + 1
+    parcel = parcel.astype(np.int8)
+    dseg = suit_atlas.data_to_nifti(parcel)
+
+    Gifti = prob_to_label_gii(probseg, altas_space, cmap, label_names)
+
+    nb.save(dseg, base_name + f"_dseg.nii")
+    nb.save(probseg, base_name + f"_probseg.nii")
+    nb.save(Gifti, base_name + "_dseg.label.gii")
+    # nt.save_lut(base_name + ".lut", np.arange(len(labels)), cmap[:, 0:4], labels)
+    print(f"Exported {base_name}.")
+
+
 def subdivde_atlas_spatial(fname,atlas,outname):
     """ Subdivides the 32 region atlas into s,i,t,v
     It performs this on the already resampled atlases in the FunctionFusion/atlas directory.
@@ -33,7 +171,8 @@ def subdivde_atlas_spatial(fname,atlas,outname):
         t: tertiary (IX/X)
         v: vermal (inferior vermis)
     Args:
-        atlas (str): Atlas name
+        fname (str): Name of map (e.g. Nettekoven32)
+        atlas (str): Atlas name (e.g. MNI152NLin2009cSymC)
     """
     sp_ext = ['s','i','t','v']
     comp = [[1,2,3,4,5,6,7,8,10],
@@ -74,12 +213,15 @@ def subdivde_atlas_spatial(fname,atlas,outname):
     parcel[sumOfProb==0]=0
     pseg_img = nb.Nifti1Image(prob_new, prob_img.affine)
     dseg_img = nb.Nifti1Image(parcel.astype(np.uint8),prob_img.affine)
+    gifti = prob_to_label_gii(pseg_img, atlas, colors_new, labels_new)
 
     out = f"atl-{outname}_space-{atlas}"
 
-    nb.save(dseg_img, tpl_dir + out + f"_space-{atlas}_dseg.nii")
-    nb.save(pseg_img, tpl_dir + out + f"_space-{atlas}_probseg.nii")
-    nt.save_lut(tpl_dir + out + ".lut", indx_new, colors_new, labels_new)
+    #Save the files for the new atlas 
+    nb.save(dseg_img, tpl_dir + f"atl-{outname}_space-{atlas}_dseg.nii")
+    nb.save(pseg_img, tpl_dir + f"atl-{outname}_space-{atlas}_probseg.nii")
+    nb.save(gifti, tpl_dir + f"atl-{outname}_dseg.label.gii")
+    nt.save_lut(tpl_dir + f"atl-{outname}.lut", indx_new, colors_new, labels_new)
 
 
 def export_conn_summary():
@@ -125,119 +267,6 @@ def save_cortex_cifti(fname):
     img = nt.make_label_cifti(par.numpy(), atlas.get_brain_model_axis())
     nb.save(img, ut.model_dir + f"/Models/{fname}.dlabel.nii")
 
-
-def export_map(data, atlas, cmap, label_names, base_name):
-    """Exports a marginal probability of a arrangement model to a Nifti (probseg), Nifti (dseg), Gifti, and lut-file.
-
-    Args:
-        data (probabilities): Marginal probabilities of the arrangement model
-        atlas (str/atlas): FunctionalFusion atlas (SUIT2,MNISym3, fs32k)
-        cmap (ListedColormap): Colormap
-        labels (list): List of labels for fields
-        base_name (_type_): File directory + basename for atlas
-    """
-    # Error if cmap and label_names are not the same length
-    if cmap.shape[0] != len(label_names):
-        raise (NameError("cmap and label_names must be the same length"))
-    # Transform cmap into numpy array
-    if not isinstance(cmap, np.ndarray):
-        cmap = cmap(np.arange(cmap.N))
-
-    suit_atlas, _ = am.get_atlas(atlas, ut.base_dir + "/Atlases")
-    probseg = suit_atlas.data_to_nifti(data)
-    parcel = np.argmax(data, axis=0) + 1
-    parcel = parcel.astype(np.int8)
-    dseg = suit_atlas.data_to_nifti(parcel)
-
-    # Figure out correct mapping space
-    if atlas[0:4] == "SUIT":
-        map_space = "SUIT"
-    elif atlas[0:7] == "MNISymC":
-        map_space = "MNISymC"
-    else:
-        raise (NameError("Unknown atlas space"))
-
-    # Plotting label
-    surf_data = suit.flatmap.vol_to_surf(probseg, stats="nanmean", space=map_space)
-    surf_parcel = np.argmax(surf_data, axis=1) + 1
-    Gifti = nt.make_label_gifti(
-        surf_parcel.reshape(-1, 1),
-        anatomical_struct="Cerebellum",
-        labels=np.arange(surf_parcel.max() + 1),
-        label_names=label_names,
-        label_RGBA=cmap,
-    )
-
-    nb.save(dseg, base_name + f"_dseg.nii")
-    nb.save(probseg, base_name + f"_probseg.nii")
-    nb.save(Gifti, base_name + "_dseg.label.gii")
-    # nt.save_lut(base_name + ".lut", np.arange(len(labels)), cmap[:, 0:4], labels)
-    print(f"Exported {base_name}.")
-
-
-def renormalize_probseg(probseg, mask):
-    """Renormalizes a probsegmentation file
-    after resampling, so that the probabilies add up to 1
-
-    Args:
-        probseg (nifti_img):
-
-    Returns:
-        probseg_img (NiftiImage): renormalize Prob segmentation
-        dseg_img (NiftiImage): desementation file
-    """
-    X = probseg.get_fdata()
-    xs = np.sum(X, axis=3)
-    X = X / np.expand_dims(xs, 3)
-    maskX = mask.get_fdata()
-    X[maskX == 0] = np.nan
-    probseg_img = nb.Nifti1Image(X, probseg.affine)
-    parcel = np.argmax(X, axis=3) + 1
-    parcel[maskX == 0] = 0
-    dseg_img = nb.Nifti1Image(parcel.astype(np.uint8), probseg.affine)
-    dseg_img.set_data_dtype("uint8")
-    dseg_img.header.set_intent(1002,(),"")
-    probseg_img.set_data_dtype("float32")
-    # probseg_img.header.set_slope_inter(1/(2**16-1),0.0)
-    return probseg_img, dseg_img
-
-
-def resample_atlas(fname, atlas="MNISymC2", target_space="MNI152NLin2009cSymC"):
-    """ Resamples probabilistic atlas from MNISymC2 to a new atlas space in 1mm resolution
-    Args:
-        fname (str): Name of the atlas
-        atlas (str/atlas): FunctionalFusion atlas (SUIT2,MNISym3, fs32k)
-        target_space (str): Target space (MNI152NLin2009cSymC, MNI152NLin2009cAsym)
-    """
-    a, ainf = am.get_atlas(atlas, ut.atlas_dir)
-    src_dir = ut.model_dir + "/Atlases/"
-    targ_dir = ut.base_dir + f"/Atlases/tpl-{target_space}"
-    # Load and set NaNs to 0
-    nii_atlas = nb.load(src_dir + f"/{fname}_space-{atlas}_probseg.nii")
-    X = np.nan_to_num(nii_atlas.get_fdata())
-    nii_atlasf = nb.Nifti1Image(X, nii_atlas.affine, nii_atlas.header)
-    # Reslice to 1mm MNI
-    print("normalizing")
-    if ainf["space"] != target_space:
-        print(f"deforming from {ainf['space']} to {target_space}")
-        deform = nb.load(
-            targ_dir + f"/tpl-{target_space}_from-{ainf['space']}_mode-image_xfm.nii"
-        )
-        nii_res = nt.deform_image(nii_atlasf, deform, 1)
-        # Get target space mask:
-        mname = f"tpl-{target_space}_res-1_gmcmask.nii"
-        nii_mask = nb.load(targ_dir + "/" + mname)
-    else:
-        # Make new shape
-        mname = ainf["mask"]
-        mname = mname.replace("res-2", "res-1")
-        nii_mask = nb.load(targ_dir + "/" + mname)
-        shap = nii_mask.shape + nii_atlas.shape[3:]
-        nii_res = ns.resample_from_to(nii_atlasf, (shap, nii_mask.affine), 1)
-    nii, dnii = renormalize_probseg(nii_res, nii_mask)
-    print("saving")
-    nb.save(nii, targ_dir + f"/atl-{fname}_space-{target_space}_probseg.nii")
-    nb.save(dnii, targ_dir + f"/atl-{fname}_space-{target_space}_dseg.nii")
 
 
 def reorder_model(
@@ -384,5 +413,5 @@ def colour_parcel(
 if __name__ == "__main__":
     # export_conn_summary()
     # export_all_probmaps()
-    subdivde_atlas_spatial(fname='NettekovenSym32',atlas='SUIT',outname='NettekovenSym128')
+    subdivde_atlas_spatial(fname='NettekovenAsym32',atlas='SUIT',outname='NettekovenAsym128')
     pass
